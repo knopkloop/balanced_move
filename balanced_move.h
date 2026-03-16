6df7b9013967724086e0ccfb760d7d5b65b80859
knopkloop <hruslan2007@gmail.com>
2026-03-16 19:48:37 +0300

    added operator[] for Vec

--- balanced_move.h
+++ balanced_move.h
@@ -6,6 +6,8 @@ struct List
 {
   T val;
   List< T >* next;
+
+  List(const T& value) : val(value), next(nullptr) {}
 };
 
 template < class T >
@@ -14,8 +16,22 @@ struct Vec
   T* data;
   size_t s;
   size_t cap;
+
+  T& operator[](size_t id)
+  {
+    return data[id];
+  }
+
+  const T& operator[](size_t id) const
+  {
+    return data[id];
+  }
 };
 
 template < class T >
-Vec< List<T>* > balanced_move(Vec< List<T>* > v, size_t k);
+Vec< List<T>* > balanced_move(Vec< List<T>* > v, size_t k)
+{
+}
+
+
 #endif
